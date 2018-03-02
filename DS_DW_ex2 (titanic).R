629126f0c43dc19a32f7a0635762f2908ac50e1a
San Miguel <tishas@amgen.com>
2018-03-02 15:17:43 -0800

    Changed the code to dplyr syntax.  I am having trouble with the last one.  Its not assigning a 0 where this is an empty cabin cell.

--- DS_DW_ex2 (titanic).R
+++ DS_DW_ex2 (titanic).R
@@ -1,24 +1,29 @@
 library(tidyverse)
 titanic <- read_csv("C:/Users/tishas/Desktop/personal/Springboard/exercises/Springboard_DataWrangling_Ex2/titanic_original.csv")
 titanic %>% glimpse
-titanic <-tbl_df(titanic)
+titanic <- tbl_df(titanic)
 View(titanic)
 
 #Find the missing values and replace them with S
-missing <- is.na(titanic$embarked)
-titanic$embarked[missing] <- "S"
+titanic <- titanic %>%
+  mutate(embarked = replace(embarked, is.na(embarked), "S")) %>%
+  mutate(embarked = replace(embarked, embarked == "", "S")) %>%
+  mutate(embarked = replace(embarked, embarked == " ", "S"))
 View(titanic)
 
 #Calculate the mean of the Age column and use that value to populate the missing values
-age <- round(mean(titanic$age, na.rm = TRUE))
-titanic$age[titanic$age == ""] <- age
+titanic <- titanic %>%
+ mutate(age = replace(age, is.na(age), mean(age, na.rm = TRUE)))
 View(titanic)
 
 #Fill these empty slots in Lifeboat with a dummy value e.g. the string 'None' or 'NA'
-titanic$boat[titanic$boat == ""] <- None
+titanic <- titanic %>%
+  mutate(boat = replace(boat, is.na(boat), "None"))
 View(titanic)
 
 #Create a new column has_cabin_number which has 1 if there is a cabin number, and 0 otherwise
-titanic <-titanic %>% 
-  mutate(has_cabin_number = ifelse(cabin == "", 0, 1))
+titanic <- titanic %>% 
+  mutate(`has cabin number` = ifelse(cabin == "", 0, 1)) %>% 
+  mutate(`has cabin number` = ifelse(cabin == " ", 0, 1)) %>% 
+  mutate(`has cabin number` = ifelse(cabin == is.na(cabin), 0, 1))
 View(titanic)
